370952b563d3194c258c3a9615a63e043acbee1f
Yan-Tavares <y.cavalcante.ae@gmail.com>
2023-11-20 13:55:13 +0100

    Sending stuff

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import 
+import 
+import 
 
 def Succes_detector(iterate_variables):
-    Succes = [True]*4
+
+
+    Succes_list = [True]*4
     if Bearing_check(...,...,...)[0] == False:
-        Succes[0] = False
+        Succes_list[0] = False
+        print("Fails by bearing check")
 
     if Pull_through_check(...,...,...)[0] == False:
-        Succes[1] = False
+        Succes_list[1] = False
+        print("Fails by pull through check")
 
     if Thermal_check(...,...,...)[0] == False:
-        Succes[2] = False
+        Succes_list[2] = False
+        print("Fails by thermal stress check")
+    return Succes_list
+
 
 
 # --------- Calculation of reaction forces 
@@ -38,11 +48,56 @@ def Succes_detector(iterate_variables):
 
 # iterate_variables = [ Material_prop_list , Fastener_material_prop_list ,  t_1 , t_2 ,  h , w , D_1 , D_2 , w_plate , h_plate , x_off , z_off ]
 
+#Material = [Youngus_Modulus,Yield_Strength,density, density]
+Aluminium_7075 = [76*10**9, 225*10**6 , 2.8*10**3]
 
 
-
+#-------- Step size configuration
 min_thickness_1 = 3*10**(-3)
 max_thickness_1 = 30*10**(-3)
+thickness_1_steps = 100
+thickness_1_stepsize = (max_thickness_1-min_thickness_1)/thickness_1_steps
+
+min_thickness_2 = 3*10**(-3)
+max_thickness_2 = 30*10**(-3)
+thickness_2_steps = 100
+thickness_2_stepsize = (max_thickness_2-min_thickness_2)/thickness_2_steps
+
+min_D_1 =
+max_D_1 =
+D_1_steps =
+D_1_step_size =
+
+min_D_2 =
+max_D_2 =
+D_2_steps =
+D_2_step_size =
+
+min_w_plate =
+max_w_plate =
+w_steps = 
+w_step_size = 
+
+min_h_plate =
+max_h_plate =
+h_steps = 
+h_step_size = 
+
+
+iterate_variables = [Material_properties]
+Valid_configurations =[]
+
+if Success_dector(iteration_variables).count(True) == 3:
+    Valid_configurations.append(iteration_variables)
+else:
+    if Succes_detector(iterate_variables)[1] == False:
+        
+
+        iterate_variables[3] = 
+        
+
+
+
 
 t_1 = min_thickness_1
 
