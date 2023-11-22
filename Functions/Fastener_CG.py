cf09088bb55fd7739274ff442c883472754c874f
Alphan Umman <ummanalphan@gmail.com>
2023-11-22 16:34:14 +0100

    Update Fastener_CG.py

--- Functions/Fastener_CG.py
+++ Functions/Fastener_CG.py
@@ -1,58 +1,61 @@
 import math as m
 
+def fastener_CG():
+    x_cg = 0
+    z_cg = 0
 
-x_cg = 0
-z_cg = 0
+    width = 0
+    h = 0
+    w = 0  # Width of the Lug
+    d_1 = 0  # Diameter of the Lug
+    d_2 = 2
+    mat = "composite"
+    nr = 4  # Fastener per row
+    nc = 2  # Column number
+    edge = 1.5 * d_2
+    sum_x = 0
+    sum_z = 0
 
-width = 0
-h = 0
-w = 0 # Width of the Lug
-d_1 = 0 # Diameter of the Lug
-d_2 = 2
-mat = "composite"
-nr = 4 # Fastener per row
-nc = 2 # Column number
-edge = 1.5*d_2
-sum = 0
+    area = (m.pi) * ((d_2) ** 2) / 4
+    x_i = []
+    z_i = []
 
-area = (m.pi) * ((d_2)**2)/4
-x_i = [] 
-z_i = []
+    for i in range(nr):
+        x_i.append(1)
+    for i in range(nc):
+        z_i.append(1)
 
-for i in range(nr):
-    x_i.append(1)
-for i in range(nc):
-   z_i.append(1)
+    for i in range(nr):
+        if i == 0 or i == nr:
+            x_i[i] = edge + d_2
+        if mat == "metal":
+            x_i[i] = x_i[i - 1] + 2 * d_2  # Says 2 OR 3 on BS.
+        elif mat == "composite":
+            x_i[i] = x_i[i - 1] + 4 * d_2  # Says 4 OR 5 on BS.
 
-for i in range(nr):
-    if i == 0 or i == nr:
-       x_i[i] = edge + d_2
-    if mat == "metal":
-       x_i[i] = x_i[i-1] + 2*d_2 ### Says 2 OR 3 on BS.
-    
-    elif mat == "composite":
-       x_i[i] = x_i[i-1] + 4*d_2 ### Says 4 OR 5 on BS.
-       
-for i in range(nc):
-    if i == 0 or i == nc:
-       z_i[i] = edge + d_2
-    if mat == "metal":
-       z_i[i] = z_i[i-1] + 2*d_2 ### Says 2 OR 3 on BS.
-    
-    elif mat == "composite":
-       z_i[i] = z_i[i-1] + 4*d_2 ### Says 4 OR 5 on BS. 
+    for i in range(nc):
+        if i == 0 or i == nc:
+            z_i[i] = edge + d_2
+        if mat == "metal":
+            z_i[i] = z_i[i - 1] + 2 * d_2  # Says 2 OR 3 on BS.
+        elif mat == "composite":
+            z_i[i] = z_i[i - 1] + 4 * d_2  # Says 4 OR 5 on BS.
 
-for i in range(nr):
-   sum = sum + x_i[i]
-x_cg = sum/nr
+    for i in range(nr):
+        sum_x = sum_x + x_i[i]
+    x_cg = sum_x / nr
 
-sum = 0
+    for i in range(nc):
+        sum_z = sum_z + z_i[i]
+    z_cg = sum_z / nc
 
-for i in range(nc): 
-   sum = sum + z_i[i] 
-z_cg = sum/nc
+    return x_i, z_i, x_cg, z_cg
 
+# Assign the return values to variables
+x_i, z_i, x_cg, z_cg = fastener_CG()
+
+# Print the results
 print("x_i locations for fasteners are", x_i)
 print("z_i locations for fasteners are", z_i)
-print("x_cg is " ,  x_cg)
-print( "z_cg is " , z_cg)
+print("x_cg is", x_cg)
+print("z_cg is", z_cg)
