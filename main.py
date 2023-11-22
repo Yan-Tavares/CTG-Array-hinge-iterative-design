9614c0090b0c5dda096623f6e7df191f90ae529f
Yan-Tavares <y.cavalcante.ae@gmail.com>
2023-11-22 15:09:15 +0100

    Lots of changes

--- main.py
+++ main.py
@@ -5,28 +5,6 @@ import
 import 
 import 
 
-def Succes_detector(iterate_variables):
-
-
-    Succes_list = [True]*4
-    if Bearing_check(...,...,...)[0] == False:
-        Succes_list[0] = False
-        print("Fails by bearing check")
-
-    if Pull_through_check(...,...,...)[0] == False:
-        Succes_list[1] = False
-        print("Fails by pull through check")
-
-    if Thermal_check(...,...,...)[0] == False:
-        Succes_list[2] = False
-        print("Fails by thermal stress check")
-    return Succes_list
-
-
-
-# --------- Calculation of reaction forces 
-
-
 # --------- Lug stresses
 #input: (n_lugs,h,t,w,D_1,F_vec,M_vec,Delta_yeld,Tau_strength)
 #output: (True/False)
@@ -49,7 +27,7 @@ def Succes_detector(iterate_variables):
 # iterate_variables = [ Material_prop_list , Fastener_material_prop_list ,  t_1 , t_2 ,  h , w , D_1 , D_2 , w_plate , h_plate , x_off , z_off ]
 
 #Material = [Youngus_Modulus,Yield_Strength,density, density]
-Aluminium_7075 = [76*10**9, 225*10**6 , 2.8*10**3]
+Aluminium_7075 = ["metal",76*10**9, 225*10**6 , 2.8*10**3]
 
 
 #-------- Step size configuration
@@ -58,34 +36,96 @@ max_thickness_1 = 30*10**(-3)
 thickness_1_steps = 100
 thickness_1_stepsize = (max_thickness_1-min_thickness_1)/thickness_1_steps
 
+min_D_1 = 
+max_D_1 =
+D_1_steps =
+D_1_step_size =
+
+min_w_1 =
+max_w_1 =
+w_1_steps = 
+w_1_stepsize = 
+
+chosen_h_1 = 0.05
+
+
+
 min_thickness_2 = 3*10**(-3)
 max_thickness_2 = 30*10**(-3)
 thickness_2_steps = 100
 thickness_2_stepsize = (max_thickness_2-min_thickness_2)/thickness_2_steps
-
-min_D_1 =
-max_D_1 =
-D_1_steps =
-D_1_step_size =
 
 min_D_2 =
 max_D_2 =
 D_2_steps =
 D_2_step_size =
 
-min_w_plate =
-max_w_plate =
-w_steps = 
-w_step_size = 
+min_w_2 =
+max_w_2 =
+w_2_steps = 
+w_2_step_size = 
 
-min_h_plate =
-max_h_plate =
+min_h_2 =
+max_h_2 =
 h_steps = 
 h_step_size = 
 
+chosen_n_r = 
+chosen_n_c = 
 
-iterate_variables = [Material_properties]
-Valid_configurations =[]
+
+
+
+
+def Succes_detector(iterate_variables):
+
+    Succes_list = [True]*4
+
+    if Lug_check(...,...,...)[0] == False:
+        Succes_list[0] = False
+        print("Fails by bearing check")
+    
+    if Bearing_check_fastener(...,...,...)[1] == False:
+        Succes_list[1] = False
+        print("Fails by bearing check")
+
+    if Pull_through_check(...,...,...)[2] == False:
+        Succes_list[2] = False
+        print("Fails by pull through check")
+
+    if Thermal_check(...,...,...)[3] == False:
+        Succes_list[3] = False
+        print("Fails by thermal stress check")
+    return Succes_list
+
+
+
+#iterate_variables = [Aluminium_7075, max_thickness_1,  max_w_1, max_D_1 , max_thickness_2 , max_D_2 , max_w_plate , max_h_plate ]
+
+#---------------- FLANGE DESIGN ----------------------------
+flange_variables = [Aluminium_7075, max_thickness_1,  max_w_1, chosen_h_1, max_D_1]
+
+# Minimal thickness
+
+while flange_variables[2] <= min_w_1:
+    flange_variables[3] = max_D_1
+
+    while flange_variables[3] <= min_D_1:
+        flange_variables[2] = max_w_1
+
+        while abs((Lug_check(...,...,...)[1])/(flange_variables[0][2]) - 1) <= 0.01 and (Lug_check(...,...,...)[1])/(flange_variables[0][2]) <= 1:
+            flange_variables[1]*(Lug_check(...,...,...)[1])/(flange_variables[0][2])
+        
+        flange_variables[3] -= D_1_step_size
+
+        if Lug_check(...,...,...)[0] == True:
+            Valid_flange.append(flange_variables)
+
+    flange_variables[2] -= w_1_stepsize
+
+#---------------- PLATE DESIGN -------------------------
+
+plate_variables = [Aluminium_7075, max_thickness_2,  max_w_2, max_h_2, max_D_2, chosen_n_r, chosen_n_c]
 
 if Success_dector(iteration_variables).count(True) == 3:
     Valid_configurations.append(iteration_variables)
